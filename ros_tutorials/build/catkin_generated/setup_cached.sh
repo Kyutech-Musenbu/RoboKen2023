e1c78094e3e70caf2c3c5141a0afdcdeab812627
Ohaginia <hamashoma@gmail.com>
2023-11-12 17:57:32 +0900

    first commit

---
+++ ros_tutorials/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export ROSLISP_PACKAGE_DIRECTORIES='/home/robot/catkin_ws/src/ros_tutorials/build/devel/share/common-lisp'
+
+# modified environment variables
+export CMAKE_PREFIX_PATH='/home/robot/catkin_ws/src/ros_tutorials/build/devel:/opt/ros/melodic:/home/robot/catkin_ws/devel'
+export LD_LIBRARY_PATH='/opt/ros/melodic/lib:/home/robot/catkin_ws/devel/lib'
+export PKG_CONFIG_PATH='/opt/ros/melodic/lib/pkgconfig:/home/robot/catkin_ws/devel/lib/pkgconfig'
+export PWD='/home/robot/catkin_ws/src/ros_tutorials/build'
+export ROS_PACKAGE_PATH="/home/robot/catkin_ws/src/ros_tutorials:$ROS_PACKAGE_PATH"
